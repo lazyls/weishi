1f27d47786788ee331b4fce9c5a550a5b16bed4e
lazyls <lazyls@yeah.net>
2018-05-02 18:13:36 +0800

    add

---
+++ Js/index.js
@@ -0,0 +1,59 @@
+﻿/// <reference path="../scripts/template-web.js" />
+/// <reference path="../scripts/jquery-3.3.1.js" />
+
+$(document).ready(function () {
+
+    !function init_view() {
+        var tmp = '\
+<div class="view_win" >\
+        <ul class="movie">\
+            {{each $data}}\
+            <li>\
+                <a href="{{$value.href}}" style="background-image:url({{$value.src}})"></a>\
+            </li >\
+            {{/each}}\
+        </ul>\
+        <ul class="thumb">\
+            {{each $data}}\
+            <li></li>\
+            {{/each}}\
+        </ul>\
+</div>';
+        var $t = $(".image_list");
+        var data = [];
+        $t.find("a").each(function () {
+            var $a = $(this);
+            var obj = {};
+            obj.href = $a.attr("href");
+            obj.src = $a.children().first().attr("src");
+            data.push(obj);
+        });
+        $t.before($(template.render(tmp, data)));
+    }();
+
+    var scroll_to = function (index) {
+        $(".movie").animate({
+            left: -index * $(".view_win").width()
+        }, 1000, "swing", function () {
+            $(".thumb").children().eq(index).addClass("active").siblings().removeClass("active");
+        })
+    };
+    $(".thumb").on("mouseenter", ">li", function () {
+        var $t = $(this);
+        scroll_to($t.index());
+    });
+    $(".movie").hover(function () {
+        var $t = $(this);
+        $t.addClass("pause");
+    }, function () {
+        var $t = $(this);
+        $t.removeClass("pause");
+    });
+
+    setInterval(function () {
+        if (!$(".movie").hasClass("pause")) {
+            var index = $(".thumb .active").index() || 0;
+            scroll_to(++index % $(".thumb").children().length);
+        }
+    }, 10000);
+})
---
+++ Js/video.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../scripts/template-web.js" />
+/// <reference path="../scripts/jquery-3.3.1.js" />
+
+$(document).ready(function () {
+
+    $("video").click(function () {
+        var $t = $(this);
+        var status = $t.data("status") || "pause";
+        if (status == "pause") {
+            $t.get(0).play();
+            $t.data("status", "play");
+        }
+        else {
+            $t.get(0).pause();
+            $t.data("status", "pause");
+        }
+    });
+})
